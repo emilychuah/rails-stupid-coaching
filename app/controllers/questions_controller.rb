ed1088ab86015aacd9cc6462e1cb3d60e0282b64
Emily Chuah <emily_earn@live.com>
2022-02-21 20:45:38 +1100

    Implemented system testing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
+# Create questions controller
 class QuestionsController < ApplicationController
   def ask; end
 
   def answer
     if params[:question].include? '?'
       @answer = 'Silly question, get dressed and go to work!'
-    elsif params[:question] == 'I am going to work right now!'
+    elsif params[:question] == 'I am going to work'
       @answer = 'Great!'
     elsif params[:question].is_a? String
       @answer = "I don't care, get dressed and go to work!"
